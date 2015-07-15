aa1565c4e1e2987034bb08e75be939a8a94c5fce
Chad Duffey <chadduffey@hotmail.com>
2015-07-15 20:46:12 +1000

    add basic style to auth page

--- app/auth_with_xero.py
+++ app/auth_with_xero.py
@@ -7,21 +7,17 @@ import requests
 import warnings
 warnings.filterwarnings("ignore")
 
-
 #Move this to env variables on heroku later.
 client_key = 'ADATDGXMOMVM1ASRTQTXDOHRUQNNO7'
 client_secret = 'F4EEJL5ZLPHAC5JJJ5IVI1IHXJHADO'
 
 def obtain_authorization_url():
-
 	request_token_url = 'https://api.xero.com/oauth/RequestToken'
 	oauth = OAuth1(client_key, client_secret=client_secret)
 	r = requests.post(url=request_token_url, auth=oauth)
-
 	credentials = parse_qs(r.content)
 	resource_owner_key = credentials.get('oauth_token')[0]
 	resource_owner_secret = credentials.get('oauth_token_secret')[0]
-
 	base_authorization_url = 'https://api.xero.com/oauth/Authorize'
 	authorize_url = base_authorization_url + '?oauth_token='
 	authorize_url = authorize_url + resource_owner_key
@@ -30,29 +26,24 @@ def obtain_authorization_url():
 
 def authorize(verifier, resource_owner_key, resource_owner_secret):
 	access_token_url = 'https://api.xero.com/oauth/AccessToken'
-
 	oauth = OAuth1(client_key,
 	                   client_secret=client_secret,
 	                   resource_owner_key=resource_owner_key,
 	                   resource_owner_secret=resource_owner_secret,
 	                   verifier=verifier)
-
 	r = requests.post(url=access_token_url, auth=oauth)
-
 	credentials = parse_qs(r.content)
-
 	try:
 		resource_owner_key = credentials.get('oauth_token')[0]
 		resource_owner_secret = credentials.get('oauth_token_secret')[0]
 	except:
 		resource_owner_key = credentials.get('oauth_problem')[0]
-		resource_owner_secret = credentials.get('oauth_problem_advice')[0]
-	
+		resource_owner_secret = credentials.get('oauth_problem_advice')[0]	
+
 	return resource_owner_key, resource_owner_secret
 
 def api_query(resource_owner_key, resource_owner_secret):
 	url = 'https://api.xero.com/api.xro/2.0/Contacts'
-
 	auth = OAuth1(client_key, 
 					client_secret=client_secret, 
 					resource_owner_key=resource_owner_key, 
--- app/forms.py
+++ app/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import Required
 from wtforms import ValidationError
 
 class TokenForm(Form):
-	token = StringField('Token', validators=[Required()])
+	token = StringField('2. Enter the XERO token here:', validators=[Required()])
 	submit = SubmitField('Authorize')
